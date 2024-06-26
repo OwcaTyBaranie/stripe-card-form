68b921564b46cbcbeb83687e739c314f7f826c55
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-06-27 01:14:16 +0200

    Add Header, Container and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import StripeCardForm from './Components/StripeCardForm/StripeCardForm';
+import styles from './App.module.scss';
+import Header from './Components/Header/Header';
+import Container from './Components/Container/Container';
+import Footer from './Components/Footer/Footer';
 
 const App = () => (
-  <div className="App">
-    <h1>Credit Card Form</h1>
+
+  <div className={styles.root}>
+  <Header />
+  <Container>
+
     <StripeCardForm />
+    </Container>
+    <Footer />
   </div>
 );
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 Credit Card Form App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
